6423b9d4b1342c24d65c786257d44bde5ce8578c
Iliodora Seferli <50150351+ISeferli@users.noreply.github.com>
2026-01-15 21:19:34 +0200

    Fix deployment error

--- App.tsx
+++ App.tsx
@@ -1,33 +1,19 @@
 
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
 import Footer from './components/Footer';
-import ProjectPage from "./pages/ProjectPage";
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-grow">
-        <Routes>
-          {/* Home */}
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <Portfolio />
-                <About />
-              </>
-            }
-          />
-          {/* Project page */}
-          <Route path="/projects/:id" element={<ProjectPage />} />
-        </Routes>
+        <Hero />
+        <Portfolio />
+        <About />
       </main>
       <Footer />
     </div>
